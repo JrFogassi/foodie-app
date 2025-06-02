cb8d9f623eb1db23de83a9ee0975b595412bb891
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-06-02 10:05:14 -0300

    ✨ Cleaning initial project, creating the  Foodie App setup to start development

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
-	return (
-		<main>
-			<h1>Morgana, i love you.</h1>
-			<p>
-				<Link href="/about">About us</Link>
-			</p>
-		</main>
-	);
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
+  );
 }
