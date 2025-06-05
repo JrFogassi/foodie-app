69b0ad9f1652debea35389f9925fd3efb4370d66
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-06-05 17:57:38 -0300

    ✨Creating the meals grid component + Setting up SQLite Database for the meals

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,3 +1,26 @@
-export default function MealsPage() {
-	return <h1>Meals Page</h1>;
+import { getMeals } from "@/lib/meals";
+import MealsGrid from "@/src/components/Meals/mealsGrid";
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default async function MealsPage() {
+	const meals = await getMeals();
+
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>
+					Delicious meals, created{" "}
+					<span className={styles.highlight}>by you</span>
+				</h1>
+				<p>Choose your favorite recipe and cook it yourself</p>
+				<p className={styles.cta}>
+					<Link href="/meals/share">Share Your Favorite Recipe</Link>
+				</p>
+			</header>
+			<main className={styles.main}>
+				<MealsGrid meals={meals} />
+			</main>
+		</>
+	);
 }
