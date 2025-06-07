4e506bbe64af6c15e638066358e2d330cd16a3ad
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-06-07 11:05:19 -0300

    ✨Adding a loading screen

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,11 +1,16 @@
 import { getMeals } from "@/lib/meals";
 import MealsGrid from "@/src/components/Meals/mealsGrid";
 import Link from "next/link";
+import { Suspense } from "react";
 import styles from "./page.module.css";
 
-export default async function MealsPage() {
+async function Meals() {
 	const meals = await getMeals();
 
+	return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
 	return (
 		<>
 			<header className={styles.header}>
@@ -13,13 +18,17 @@ export default async function MealsPage() {
 					Delicious meals, created{" "}
 					<span className={styles.highlight}>by you</span>
 				</h1>
-				<p>Choose your favorite recipe and cook it yourself</p>
+				<p>Choose your favorite recipe and cook it yourself.</p>
 				<p className={styles.cta}>
 					<Link href="/meals/share">Share Your Favorite Recipe</Link>
 				</p>
 			</header>
 			<main className={styles.main}>
-				<MealsGrid meals={meals} />
+				<Suspense
+					fallback={<p className={styles.loading}>Fetching meals...</p>}
+				>
+					<Meals />
+				</Suspense>
 			</main>
 		</>
 	);
