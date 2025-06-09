9f57c44b3e0d23e9a782977e393abafd7a3d29a5
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-06-09 11:27:24 -0300

    ⚒️Handling error messages

---
+++ src/app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+export default function Error({ error }) {
+	return (
+		<main className="error">
+			<h1>An error occurred!</h1>
+			<p>Failed to fetch meal data. Please try again later.</p>
+		</main>
+	);
+}
